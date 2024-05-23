cd68b57b7b2c06b5f16ec28d34706164ddd1c0b3
DirkQueck <76432409+D-Queck@users.noreply.github.com>
2024-05-23 15:59:45 +0200

    implement kasse features

---
+++ techmax/shop/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from . models import *
+
+# Create your views here.
+
+def shop(request):
+    artikels = Artikel.objects.all()
+    ctx = {'artikels':artikels}
+    return render(request, 'shop/shop.html', ctx)
+
+def warenkorb(request):
+    if request.user.is_authenticated:
+        kunde = request.user.kunde
+        bestellung, created = Bestellung.objects.get_or_create(kunde=kunde, erledigt=False)
+        artikels = bestellung.bestellteartikel_set.all()
+    else:
+        artikels = []
+        bestellung = []
+
+    ctx = {"artikels":artikels, "bestellung":bestellung}        
+    return render(request, 'shop/warenkorb.html',ctx)
+
+def kasse(request):
+    if request.user.is_authenticated:
+        kunde = request.user.kunde
+        bestellung, created = Bestellung.objects.get_or_create(kunde=kunde, erledigt=False)
+        artikels = bestellung.bestellteartikel_set.all()
+    else:
+        artikels = []
+        bestellung = []
+
+    ctx = {"artikels":artikels, "bestellung":bestellung}      
+    return render(request, 'shop/kasse.html', ctx)
+    
+    
